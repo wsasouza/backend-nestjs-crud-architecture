bb05cd66c47abcbedfb37c9c52592a0b23370a2d
wsasouza <wsasouza@hotmail.com>
2021-12-08 15:09:43 -0300

    Feat: Implement user validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
+import { IsEmail, IsString } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends User {
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
 }
