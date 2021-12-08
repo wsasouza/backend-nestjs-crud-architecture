13476957aab60d0d39619d25b63ef190edc407df
wsasouza <wsasouza@hotmail.com>
2021-12-08 13:04:20 -0300

    Feat: Implement endpoint create user

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  name: string;
+  email: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Administrator System',
+      email: 'admin@admin.com.br',
+    },
+  ];
+
+  create(createUserDto: CreateUserDto) {
+    const id = this.users[this.users.length - 1].id + 1;
+
+    const user: User = {
+      id,
+      ...createUserDto,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
